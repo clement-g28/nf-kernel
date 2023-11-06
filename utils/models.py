ff261871e48f9513dffb0b27ad8c8327f7b8de09
clement-g28 <clement.gledel@gmail.com>
2023-11-06 15:44:53 +0100

    test gpu memory leak

--- utils/models.py
+++ utils/models.py
@@ -966,6 +966,8 @@ class CMoFlow(NF):
                     tmp_dataset.transform = None
 
         create_animation(dir=f'{save_dir}/interpretation_transformation', how_much_repeat=10)
+        tmp_dataset.cpu()
+        del tmp_dataset
 
     def forward(self, input, label, pair_with_noise=False):
         x, adj = input
