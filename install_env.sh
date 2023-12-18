213603ff99f7bdfcc9ecf3b4d107a17028373b8a
clement-g28 <clement.gledel@gmail.com>
2023-12-18 18:11:34 +0100

    fix name

--- install_env.sh
+++ install_env.sh
@@ -1,4 +1,4 @@
-conda env create -f envronement.yml
+conda env create -f environement.yml
 conda activate py39
 conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=10.2 -c pytorch
 pip install chardet
